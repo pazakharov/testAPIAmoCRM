ecfdf6e41ae3c5d0137b5e79722215f1c8f6a46d
redesc <p.a.zakharov@gmail.com>
2021-01-07 22:10:09 +0800

    fetch

--- amo.php
+++ amo.php
@@ -5,6 +5,7 @@ use AmoCRM\Filters\ContactsFilter;
 use AmoCRM\Collections\TasksCollection;
 use AmoCRM\Helpers\EntityTypesInterface;
 use AmoCRM\Exceptions\AmoCRMApiException;
+use AmoCRM\Collections\ContactsCollection;
 use League\OAuth2\Client\Token\AccessTokenInterface;
 
 include_once __DIR__ . '/bootstrap.php';
@@ -28,17 +29,51 @@ $apiClient->setAccessToken($accessToken)
 
 $contactsService = $apiClient->contacts();
 $contactsFilter = new ContactsFilter();
-$contactsFilter->setLimit(API_PER_PAGE_LIMIT);
+//$contactsFilter->setLimit(API_PER_PAGE_LIMIT);
+$contactsFilter->setLimit(10);
 
 $tasksService = $apiClient->tasks();
 $tasksCollection = new TasksCollection();
+$contactsCollection = new ContactsCollection();
 
-try {
-	$contactsCollection = $contactsService->get($contactsFilter, ['leads']);
-} catch (AmoCRMApiException $e) {
-	printError($e);
-	die;
+function fetchAllEntities($collection, $service, $filter, $next = FALSE, $with = ['leads'])
+{
+	sleep(1);
+	$fetchedCollection = new ContactsCollection();
+	if ($next == TRUE) {
+		try {
+			print_r($collection->count());
+			$fetchedCollection = $service->nextPage($collection);
+			
+		} catch (AmoCRMApiException $e) {
+			printError($e);
+			//die;
+		}
+	} else {
+		try {
+			$fetchedCollection = $service->get($filter, $with);
+		} catch (AmoCRMApiException $e) {
+			printError($e);
+			//die;
+		}
+	}
+
+	foreach ($fetchedCollection as $entity) {
+		$collection->add($entity);
+	}
+	//print_r($fetchedCollection->getNextPageLink());
+	if ($fetchedCollection->getNextPageLink()) {
+		fetchAllEntities($fetchedCollection, $service, $filter,TRUE);
+	}
+	print_r($collection->count());
+
+	return $collection;
 }
+
+$contactsCollection = fetchAllEntities($contactsCollection, $contactsService, $contactsFilter);
+echo ($contactsCollection->count());
+die;
+
 
 foreach ($contactsCollection as $contactObj) {
 	if (!$contactObj->leads) {
