00ce8d099798ab7571088fee27767cdbdc3205d4
redesc <p.a.zakharov@gmail.com>
2021-01-08 01:16:22 +0800

    with fetching page by page

--- amo.php
+++ amo.php
@@ -4,11 +4,54 @@ use AmoCRM\Models\TaskModel;
 use AmoCRM\Filters\ContactsFilter;
 use AmoCRM\Collections\TasksCollection;
 use AmoCRM\Helpers\EntityTypesInterface;
+use AmoCRM\Collections\BaseApiCollection;
 use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Collections\ContactsCollection;
 use League\OAuth2\Client\Token\AccessTokenInterface;
 
 include_once __DIR__ . '/bootstrap.php';
+
+/**
+ * Функция возвращает коллекцию всех сущностей
+ * @param mixed $collection
+ * @param mixed $service
+ * @param mixed $filter
+ * @param  $next 
+ * @param array $with
+ * 
+ * @return [type]
+ */
+function fetchAllEntities($collection, $service, $filter, $with, $next = FALSE)
+{
+	sleep(1);
+	if ($next == TRUE) {
+		try {
+			$fetchedCollection = $service->nextPage($collection);
+		} catch (AmoCRMApiException $e) {
+			//printError($e);
+			//die;
+		}
+	} else {
+		try {
+			$fetchedCollection = $service->get($filter, $with);
+		} catch (AmoCRMApiException $e) {
+			printError($e);
+			die;
+		}
+	}
+
+	if (isset($fetchedCollection) && ($fetchedCollection instanceof BaseApiCollection)) {
+		foreach ($fetchedCollection as $entity) {
+			$collection->add($entity);
+		}
+		if (method_exists($fetchedCollection, 'getNextPageLink') && $fetchedCollection->getNextPageLink()) {
+			$collection->setNextPageLink($fetchedCollection->getNextPageLink());
+			$collection = fetchAllEntities($collection, $service, $filter, $with, TRUE);
+		}
+	}
+
+	return $collection;
+}
 
 $accessToken = getToken();
 
@@ -30,50 +73,13 @@ $apiClient->setAccessToken($accessToken)
 $contactsService = $apiClient->contacts();
 $contactsFilter = new ContactsFilter();
 //$contactsFilter->setLimit(API_PER_PAGE_LIMIT);
-$contactsFilter->setLimit(10);
+$contactsFilter->setLimit(2);
 
 $tasksService = $apiClient->tasks();
 $tasksCollection = new TasksCollection();
 $contactsCollection = new ContactsCollection();
 
-function fetchAllEntities($collection, $service, $filter, $next = FALSE, $with = ['leads'])
-{
-	sleep(1);
-	$fetchedCollection = new ContactsCollection();
-	if ($next == TRUE) {
-		try {
-			print_r($collection->count());
-			$fetchedCollection = $service->nextPage($collection);
-			
-		} catch (AmoCRMApiException $e) {
-			printError($e);
-			//die;
-		}
-	} else {
-		try {
-			$fetchedCollection = $service->get($filter, $with);
-		} catch (AmoCRMApiException $e) {
-			printError($e);
-			//die;
-		}
-	}
-
-	foreach ($fetchedCollection as $entity) {
-		$collection->add($entity);
-	}
-	//print_r($fetchedCollection->getNextPageLink());
-	if ($fetchedCollection->getNextPageLink()) {
-		fetchAllEntities($fetchedCollection, $service, $filter,TRUE);
-	}
-	print_r($collection->count());
-
-	return $collection;
-}
-
-$contactsCollection = fetchAllEntities($contactsCollection, $contactsService, $contactsFilter);
-echo ($contactsCollection->count());
-die;
-
+$contactsCollection = fetchAllEntities($contactsCollection, $contactsService, $contactsFilter, ['leads']);
 
 foreach ($contactsCollection as $contactObj) {
 	if (!$contactObj->leads) {
